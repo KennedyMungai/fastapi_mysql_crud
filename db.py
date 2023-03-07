72d0d2adaf008e43b22c4afd1fab3fb70890899a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-07 12:55:41 +0300

    Added a try except block for connection

--- db.py
+++ db.py
@@ -2,16 +2,27 @@
 import os
 
 import pymysql
+from pymysql import Error
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
 connection = pymysql.connect(
     host=os.environ.get("MYSQL_HOST"),
-    port=os.environ.get("MYSQL_PORT"),
+    port=int(os.environ.get("MYSQL_PORT")),
     user=os.environ.get("MYSQL_USER"),
     password=os.environ.get("MYSQL_PASSWORD"),
     db=os.environ.get("MYSQL_DB"),
 )
 
-cursor = connection.cursor()
+try:
+    cursor = connection.cursor()
+    cursor.execute('select database();')
+    db = cursor.fetchone()
+    print("You are connected to the database: ", db)
+except Error:
+    print("Error occurred while connecting to MySQL db", e)
+finally:
+    cursor.close()
+    connection.close()
+    print("MySQL connection is closed")
