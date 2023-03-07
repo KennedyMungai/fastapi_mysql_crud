5eb6d17f453faf67c418217687d0c2feb3626813
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-07 13:09:26 +0300

    Imported users

--- models/user.py
+++ models/user.py
@@ -1,3 +1,13 @@
 """A file containing the user model"""
 from sqlalchemy import Table, Column
 from config.db import meta
+from sqlalchemy.sql.sqltypes import Integer, String
+
+
+users = Table(
+    'users', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(255)),
+    Column('email', String(255)),
+    Column('password', String(255)),
+)
