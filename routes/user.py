6ef94b71f8059fd9a119ad82fc5dabc89a29589b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-07 13:40:46 +0300

    Changed the name of the function

--- routes/user.py
+++ routes/user.py
@@ -8,4 +8,22 @@ user = APIRouter()
 
 @user.get("/")
 async def read_data():
+    """The root endpoint for the users
+
+    Returns:
+        connection data: The result of a db query
+    """
     return connection.execute(users.select()).fetchall()
+
+
+@user.get("/{id}")
+async def read_data_with_id(id: int):
+    """Reading the data of a specific user with the given id
+
+    Args:
+        id (int): The id of the user
+
+    Returns:
+        Query Result: THe result of the executed query
+    """
+    return connection.execute(users.select().where(users.c.id == id)).fetchall()
