1003f5ef704f5445777124552ea641eceda7e5f3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-07 13:37:20 +0300

    Created the root route for the user

--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,11 @@
 """The file containing the User route"""
 from fastapi import APIRouter
+from config.db import connection
+from models.index import users
 
 user = APIRouter()
 
 
 @user.get("/")
 async def read_data():
-    return {"user": "data"}
+    return connection.execute(users.select()).fetchall()
