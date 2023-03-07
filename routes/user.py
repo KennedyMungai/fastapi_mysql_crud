c5a94ae44881a9c203621b6498ae9c42af7e8133
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-07 13:46:01 +0300

    Finished writing the write_data function

--- routes/user.py
+++ routes/user.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter
 from config.db import connection
 from models.index import users
+from schemas.user import User
 
 user = APIRouter()
 
@@ -27,3 +28,22 @@ async def read_data_with_id(id: int):
         Query Result: THe result of the executed query
     """
     return connection.execute(users.select().where(users.c.id == id)).fetchall()
+
+
+@user.post('/')
+async def write_data(user: User):
+    """A function to write data to teh database
+
+    Args:
+        user (User): User data based on the User template
+
+    Returns:
+        Query Result: Returns the result of the executed SQL query
+    """
+    connection.execute(users.insert().values(
+        name=user.name,
+        email=user.email,
+        password=user.password
+    )).fetchall()
+
+    return connection.execute(users.select()).fetchall()
