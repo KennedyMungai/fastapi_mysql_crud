425968338ad36815da15ccfd4e1bc3bddc6756c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-07 13:49:18 +0300

    Added a docstring for the User class

--- schemas/user.py
+++ schemas/user.py
@@ -3,6 +3,11 @@ from pydantic import BaseModel
 
 
 class User(BaseModel):
+    """The template for the user data
+
+    Args:
+        BaseModel (Parent class): The User class inherits the BaseModel class
+    """
     id: int
     name: str
     email: str
