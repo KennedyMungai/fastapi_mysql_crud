7dd8c4ff495db356fc68c5d8f5d84115f2aa7819
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-07 11:24:43 +0300

    Created the connecion to a database

--- db.py
+++ db.py
@@ -1,2 +1,15 @@
 """Created the file to handle the db connection"""
+import os
+
 import MySQLdb
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+connection = MySQLdb.connect(
+    host=os.environ.get("MYSQL_HOST"),
+    port=os.environ.get("MYSQL_PORT"),
+    user=os.environ.get("MYSQL_USER"),
+    password=os.environ.get("MYSQL_PASSWORD"),
+    db=os.environ.get("MYSQL_DB"),
+)
